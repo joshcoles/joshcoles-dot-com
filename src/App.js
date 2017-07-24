9e519ef42287eaed1ea4f37cb6f9003c75526a68
Josh Coles <josh@forgeandsmith.com>
2017-07-23 19:25:08 -0700

    Renamed intro section component + broke it out of App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import ProfileSection from './components/ProfileSection';
+import IntroSection from './components/IntroSection';
 
 
 class App extends Component {
@@ -9,19 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="site-header">
-
           <ProfileSection />
         </div>
-        <section className="homepage-section homepage-intro">
-          <div className="intro-bio">
-            <h3>Hi, I'm Josh.</h3>
-            <p>I'm a junior web developer living in Vancouver, BC. I moved here in the fall of 2016 to take a full-stack web development bootcamp at <a target="_blank" rel="noopener noreferrer" href="https://www.lighthouselabs.ca/">Lighthouse Labs</a>.</p>
-            <p>Since graduating I've done mostly front-end work on Wordpress sites for <a target="_blank" rel="noopener noreferrer" href="http://pivotandpilot.com">Pivot & Pilot</a> and <a target="_blank" rel="noopener noreferrer" href="http://forgeandsmith.com">Forge and Smith</a>, but I'm interested in working more in React and with other Javascript libraries. I'm going to use this site to practice my react skills.</p>
-            <p>You can find my up-to-date resume <a target="_blank" rel="noopener noreferrer" href="http://hire.joshcoles.com">here</a>.</p>
-            <p>You can send me an email at <a href="mailto:josh@joshcoles.com" className="email-address">josh@joshcoles.com</a>.</p>
-
-          </div>
-        </section>
+        <IntroSection />
       </div>
     );
   }
---
+++ src/components/IntroSection.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class IntroSection extends React.Component {
+  render() {
+    return (
+      <section className="homepage-section homepage-intro">
+          <div className="intro-bio">
+            <h3>Hi, I'm Josh.</h3>
+            <p>I'm a junior web developer living in Vancouver, BC. I moved here in the fall of 2016 to take a full-stack web development bootcamp at <a target="_blank" rel="noopener noreferrer" href="https://www.lighthouselabs.ca/">Lighthouse Labs</a>.</p>
+            <p>Since graduating I've done mostly front-end work on Wordpress sites for <a target="_blank" rel="noopener noreferrer" href="http://pivotandpilot.com">Pivot & Pilot</a> and <a target="_blank" rel="noopener noreferrer" href="http://forgeandsmith.com">Forge and Smith</a>, but I'm interested in working more in React and with other Javascript libraries. I'm going to use this site to practice my react skills.</p>
+            <p>You can find my up-to-date resume <a target="_blank" rel="noopener noreferrer" href="http://hire.joshcoles.com">here</a>.</p>
+            <p>You can send me an email at <a href="mailto:josh@joshcoles.com" className="email-address">josh@joshcoles.com</a>.</p>
+          </div>
+        </section>
+    );
+  }
+}
+
+
+export default IntroSection;
