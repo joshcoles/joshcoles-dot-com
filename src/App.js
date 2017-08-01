8480387d3a3dfd8f76809175797c8edc85b244f0
Josh Coles <josh@forgeandsmith.com>
2017-07-31 20:34:06 -0700

    Cleaned out a bunch of routing junk, going to approach it cleanly again.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
 
 import './App.css';
 import Header from './components/Header';
 import IntroSection from './components/IntroSection';
-import Home from './components/Home';
-import Nav from './components/Nav';
 
 
-class App extends Component {
+
+
+class App extends React.Component {
   render() {
+
     return (
         <div className="App">
           <div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SocialRow from './SocialRow';
 import logo from '../profile-picture.jpeg';
-import Nav from './Nav';
-import Home from './Home';
-import App from '../App';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class ProfileSection extends React.Component {
 
